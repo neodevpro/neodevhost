21d6cb5a65859d2ba53f288e3a9681d839d23373
Fusion <leemenhin@gmail.com>
2020-05-21 04:30:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+echo -e "Adding NEODEV AD TRACKING BLOCK HOST..."
+pihole -b https://raw.githubusercontent.com/neodevpro/neodevhost/master/hosts.txt 
+
 echo -e "Downloading Whitelist..."
 curl -sS https://raw.githubusercontent.com/neodevpro/neodevhost/master/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
 sleep 0.5
@@ -6,4 +9,6 @@ mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/wh
 wait
 echo -e "Patching(Need some time)..."
 pihole -w -q $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
+echo -e "Upate PiHole Gravity..."
+pihole -g
 echo -e "Done!"
