ecde837e0d28b48108fecaa1eb0c8a039593d9e7
Fusion <leemenhin@gmail.com>
2020-05-21 04:43:23 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,4 @@
-echo -e "Removing(Need some time)..."
-sed -i '/##NEO DEV HOST/d' /etc/pihole/adlists.list  
-sed -i '/https://raw.githubusercontent.com/neodevpro/neodevhost/master/hosts.txt/d' /etc/pihole/adlists.list  
+echo -e "Removing(Need some time)..." 
 pihole -w -d $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 echo -e "Update Gravity..."
 pihole -g
