a55443f86e9c30ef709b5a4e084484cc190580b9
Fusion <leemenhin@gmail.com>
2020-05-21 04:56:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
+echo -e "Downloading NEO DEV HOST ..."
+curl -sS https://raw.githubusercontent.com/neodevpro/neodevhost/master/hosts.txt | sudo tee -a /etc/pihole/hosts.txt >/dev/null
+sleep 0.5
+echo -e "Editing..."
+mv /etc/pihole/hosts.txt /etc/pihole/hosts.txt.old && cat /etc/pihole/hosts.txt.old | sort | uniq >> /etc/pihole/hosts.txt
+sed -i 's/127.0.0.1 //' /etc/pihole/hosts.txt
+sed -i '/#/d' /etc/pihole/hosts.txt
+sed -i '/255.255.255.255/d' /etc/pihole/hosts.txt
+sed -i '/::1/d' /etc/pihole/hosts.txt
+sed -i '/ ip6-/d' /etc/pihole/hosts.txt
+sed -i '/0.0.0.0/d' /etc/pihole/hosts.txt
+sed -i '/fe80::/d' /etc/pihole/hosts.txt
+
+echo -e "Patching(Need some time)..."
+pihole -b $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
+
 echo -e "Downloading Whitelist..."
 curl -sS https://raw.githubusercontent.com/neodevpro/neodevhost/master/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
 sleep 0.5
