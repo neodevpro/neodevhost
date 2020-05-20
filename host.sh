77218d75ea438584f202aa3db3962e7dbce9d464
Fusion <leemenhin@gmail.com>
2020-05-21 00:17:51 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -75,6 +75,7 @@ sed -i 's/，腾讯视频网页下一集按钮灰色，也不能选集播放//' 
 sed -i 's/会导致腾讯动漫安卓版的逗比商城白屏//' $whitelist
 sed -i 's/REG ^//' $whitelist
 sed -i 's/RZD //' $whitelist
+sed -i 's/address=/.bcebos.com/0.0.0.0 baidu maps //' $whitelist
 sed -i 's/ALL .//' $whitelist
 sed -e "s/^[ \t]*//g" -e "s/[ \t]*$//g" -e "s/\r//g" -e "/^$/d" -e 's/^/127.0.0.1 &/g' $whitelist
 sed -i '/^$/d' $whitelist
