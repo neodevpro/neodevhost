21df1e22df51e6e2d74f242f0ac463108442f2a0
Fusion <leemenhin@gmail.com>
2020-05-20 19:43:27 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -51,6 +51,9 @@ EOF
 echo " "
 echo "Geanera AD host file..."
 wait
+sort -n $host | uniq
+sort -n $host | awk '{if ($0!=line) print;line=$0}'
+sort -n $host | sed '$!N; /^.∗\n\1$/!P; D'
 sed -i '/^#/'d $host
 sed -i '/</d' $host
 sed -i '/>/d' $host
@@ -62,11 +65,12 @@ sed -i 's/0.0.0.0 //' $host
 sed -i "s/http:\/\///" $host
 sed -i "s/https:\/\///" $host
 sed -i "s/localhost//" $host
+sed -i "s/ip6-//" $host
 sed -i "s/ip6-loopback//" $host
 sed -i "s/ip6-localhost//" $host
 #sed -i -e "s/^[ \t]*//g" -e "s/[ \t]*$//g" -e "s/\r//g" -e "s/[ \t][ \t]*/ /g" -e "/^$/d" $host
 #sed -i '/^\(127\|0\|::\)/!d;s/0.0.0.0/127.0.0.1/g;/ip6-/d;/localhost/d;s/#.*//g;s/\s\{2,\}//g' $host
-sed 's/^/127.0.0.1 &/g' $host
+sed -i 's/^/127.0.0.1 &/g' $host
 sort -n $host | uniq
 sort -n $host | awk '{if ($0!=line) print;line=$0}'
 sort -n $host | sed '$!N; /^.∗\n\1$/!P; D'
@@ -74,6 +78,9 @@ sort -n $host | sed '$!N; /^.∗\n\1$/!P; D'
 echo " "
 echo "Geanera whitelist..."
 wait
+sort -n $host | uniq
+sort -n $host | awk '{if ($0!=line) print;line=$0}'
+sort -n $host | sed '$!N; /^.∗\n\1$/!P; D'
 sed -i '/^#/'d $whitelist
 sed -i 's/127.0.0.1 //' $whitelist
 sed -i "s/http:\/\///" $whitelist
