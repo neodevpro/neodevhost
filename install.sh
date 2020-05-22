d3042a7a6811b0829d1d5ffba8aace313578d7ad
Fusion <leemenhin@gmail.com>
2020-05-22 10:44:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 echo -e "Downloading Whitelist..."
-curl -sS https://raw.githubusercontent.com/neodevpro/neodevhost/master/whitelist.txt
+curl -sS https://raw.githubusercontent.com/neodevpro/neodevhost/master/whitelist
 sleep 0.5
 echo -e "Editing..."
-mv ./whitelist.txt ./whitelist.txt.old && cat ./whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+mv ./whitelist ./whitelist.old && cat ./whitelist.old | sort | uniq >> /etc/pihole/whitelist
 wait
 echo -e "Patching(Need some time)..."
 pihole -w -q $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
