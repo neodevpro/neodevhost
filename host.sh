0f4760ab04b191ab8f70aab363b7711a818ab51f
Fusion <leemenhin@gmail.com>
2021-12-19 23:06:33 +0800

    Add Domain compatibility

--- host.sh
+++ host.sh
@@ -3,7 +3,7 @@
 echo " "
 echo "Clean..."
 
-rm -f host lite_host lite_adblocker adblocker lite_dnsmasq.conf dnsmasq.conf deadallow deadblock checkblock checkallow smartdns.conf lite_smartdns.conf
+rm -f host lite_host lite_adblocker adblocker lite_dnsmasq.conf dnsmasq.conf deadallow deadblock checkblock checkallow smartdns.conf lite_smartdns.conf doamin lite_domain
 
 echo " "
 echo "Merge allow..."
@@ -113,10 +113,12 @@ echo "Adding Compatibility..."
 cp host adblocker
 cp host dnsmasq.conf
 cp host smartdns.conf
+cp host domain
 
 cp lite_host lite_adblocker
 cp lite_host lite_dnsmasq.conf
 cp lite_host lite_smartdns.conf
+cp lite_host lite_domain
 
 
 sed -i 's/^/||&/' adblocker
@@ -154,8 +156,8 @@ sed -i '20cTotal deadblock list 失效屏蔽广告域名： '$(wc -l deadblock)'
 sed -i '22cTotal deadallow list 失效允许广告域名： '$(wc -l deadallow)' ' README.md
 sed -i '24cUpdate 更新时间: '$(date "+%Y-%m-%d")'' README.md
 
-sed -i '54cNumber of Domain 域名数目： '$(wc -l host)' ' README.md
-sed -i '65cNumber of Domain 域名数目： '$(wc -l lite_host)' ' README.md
+sed -i '54cNumber of Domain 域名数目： '$(wc -l domain)' ' README.md
+sed -i '65cNumber of Domain 域名数目： '$(wc -l lite_domain)' ' README.md
 
  
 cp title title.2
@@ -169,7 +171,13 @@ sed -i '9c# Last update: '$(date "+%Y-%m-%d")'' title.6
 sed -i '11c# Number of blocked domains:  '$(wc -l dnsmasq.conf)' ' title.6
 cp title title.8
 sed -i '9c# Last update: '$(date "+%Y-%m-%d")'' title.8
-sed -i '11c# Number of blocked domains:  '$(wc -l smartdns.conf)' ' title.8            
+sed -i '11c# Number of blocked domains:  '$(wc -l smartdns.conf)' ' title.8    
+cp title title.10
+sed -i '9c# Last update: '$(date "+%Y-%m-%d")'' title.10
+sed -i '11c# Number of blocked domains:  '$(wc -l domain)' ' title.10             
+
+
+
 
 cp title title.1
 sed -i '9c# Last update: '$(date "+%Y-%m-%d")'' title.1
@@ -183,29 +191,36 @@ sed -i '11c# Number of blocked domains:  '$(wc -l lite_dnsmasq.conf)' ' title.5
 cp title title.7
 sed -i '9c# Last update: '$(date "+%Y-%m-%d")'' title.7
 sed -i '11c# Number of blocked domains:  '$(wc -l lite_smartdns.conf)' ' title.7  
+cp title title.9
+sed -i '9c# Last update: '$(date "+%Y-%m-%d")'' title.9
+sed -i '11c# Number of blocked domains:  '$(wc -l lite_domain)' ' title.9  
 
 
 cat host >>title.2
 cat adblocker >>title.4
 cat dnsmasq.conf >>title.6
 cat smartdns.conf >>title.8
+cat domain >>title.10
 
 cat lite_host >>title.1
 cat lite_adblocker >>title.3
 cat lite_dnsmasq.conf >>title.5
 cat lite_smartdns.conf >>title.7
+cat lite_domain >>title.9
 
-rm -f host adblocker dnsmasq.conf lite_host lite_adblocker lite_dnsmasq.conf deadallow deadblock allow lite_block block smartdns.conf lite_smartdns.conf
+rm -f host adblocker dnsmasq.conf lite_host lite_adblocker lite_dnsmasq.conf deadallow deadblock allow lite_block block smartdns.conf lite_smartdns.conf doamin lite_domain
 
 mv title.2 host
 mv title.4 adblocker
 mv title.6 dnsmasq.conf
 mv title.8 smartdns.conf
+mv title.10 domain
 
 mv title.1 lite_host
 mv title.3 lite_adblocker
 mv title.5 lite_dnsmasq.conf
 mv title.7 lite_smartdns.conf
+mv title.9 lite_domain
 
 echo " "
 echo "Done!"
