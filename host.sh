369658366081da89a4e8cf8d388cf50927f8c9d6
Fusion <leemenhin@gmail.com>
2022-10-15 18:01:29 +0800

    Update host.sh

--- host.sh
+++ host.sh
@@ -63,6 +63,7 @@ sed -i 's/127.0.0.1 //' tmpblock
 
 sed -i '/:/d' tmpblock
 sed -i '/!/d' tmpblock
+sed -i '/|/d' tmpblock
 sed -i '/^$/d' tmpblock
 sed -i s/[[:space:]]//g tmpblock
 sort -u tmpblock > block
