20eeb72ab6fb631cea1e2ce45b5f7ddd70b90865
Fusion <leemenhin@gmail.com>
2025-02-19 07:44:09 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,48 +1,30 @@
 #!/bin/bash
 
-tmp_allow=$(mktemp)
-tmp_block=$(mktemp)
-
 # Clean up old files
 echo "Clean..."
 rm -f host adblocker dnsmasq.conf smartdns.conf domain clash allow block
 
-# Merge allowlist
-echo "Merging allowlist..."
-while read -r url; do
-    [[ -z "$url" || "$url" =~ ^# ]] && continue
-    wget --no-check-certificate -t 1 -T 10 -q -O - "$url" >> "$tmp_allow"
-done < allowlist
+# Merge list
+merge_list() {
+    local input_file=$1
+    local output_file=$2
+    local temp_file=$(mktemp)
 
-sed -i -e '/#/d' -e 's/[[:space:]]//g' "$tmp_allow"
-sort -u "$tmp_allow" > allow
+    echo "Merging $input_file into $output_file..."
+    while read -r url; do
+        [[ -z "$url" || "$url" =~ ^# ]] && continue
+        wget --no-check-certificate -t 1 -T 10 -q -O - "$url" >> "$temp_file"
+    done < "$input_file"
 
+    # Cleanup
+    grep -E "^[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}(:[0-9]+)?([/?].*)?$" "$temp_file" | sort -u > "$output_file"
+    sed -i -e '/#/d' -e 's/[[:space:]]//g' "$output_file"
+    rm -f "$temp_file"
+}
 
-# Merge blocklist
-echo "Merging blocklist..."
-while read -r url; do
-    [[ -z "$url" || "$url" =~ ^# ]] && continue
-    wget --no-check-certificate -t 1 -T 10 -q -O - "$url" >> "$tmp_block"
-done < blocklist
-
-sed -i -e '/#/d' -e '/@/d' -e '/^$/d' -e 's/[[:space:]]//g' "$tmp_block"
-sed -i '/^\(127\.0\.0\.1 localhost\|127\.0\.0\.1 localhost\.localdomain\|127\.0\.0\.1 local\|255\.255\.255\.255 broadcasthost\|::1 localhost\|::1 ip6-localhost\|::1 ip6-loopback\|fe80::1%lo0 localhost\|ff00::0 ip6-localnet\|ff00::0 ip6-mcastprefix\|ff02::1 ip6-allnodes\|ff02::2 ip6-allrouters\|ff02::3 ip6-allhosts\|0\.0\.0\.0 0\.0\.0\.0\)$/d' "$tmp_block"
-
-sort -u "$tmp_block" > block
-
-
-# Check format
-echo "Check format..."
-sed -E -e '/^[^[:space:]]+\.[^[:space:]]+$/!d' allow
-sed -E -e '/^[^[:space:]]+\.[^[:space:]]+$/!d' block
-
-domain_regex="^[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}(:[0-9]+)?([/?].*)?$"
-
-grep -E "$domain_regex" allow > cleanallow
-grep -E "$domain_regex" block > cleanblock
-
-mv cleanallow allow
-mv cleanblock block
+# Handle allowlist 和 blocklist
+merge_list "allowlist" "allow"
+merge_list "blocklist" "block"
 
 
 # Generate final lite host list
@@ -50,7 +32,6 @@ echo "Merge Combine..."
 sort -u block allow > host
 sed -i '/^$/d' host
 sed -i s/[[:space:]]//g host
-
 
 
 # Create lists
@@ -94,6 +75,3 @@ sed -i "48cNumber of Domain 域名数目： $(wc -l < block)" README.md
 
 echo " "
 echo "Done!"
-
-# Cleanup
-rm -f "$tmp_allow" "$tmp_block"
