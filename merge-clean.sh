51b1e556595666a9f32f8acd358f1a60a8754a78
Fusion <leemenhin@gmail.com>
2025-02-19 05:07:26 +0000

    Update merge-clean.sh

--- merge-clean.sh
+++ merge-clean.sh
@@ -1,13 +1,7 @@
 #!bin/bash
-
 echo " "
 echo "Clean..."
-
 sort -u customallowlist > customallowlist
-
-
-sort -u customblocklist%0D > customblocklist
-
-
+sort -u customblocklist > customblocklist
 echo " "
 echo "Done..."
