5d5a016b1ceb97e3742e8cf4d690310900b81f2b
Fusion <leemenhin@gmail.com>
2025-02-20 14:00:15 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -17,7 +17,9 @@ process_list() {
 }
 
 # Run allowlist and blocklist processing concurrently
-process_list "allowlist" "allow" & process_list "blocklist" "block" & wait
+process_list "allowlist" "allow"
+process_list "blocklist" "block" 
+wait
 
 # Check format
 echo "Validating domain format..."
