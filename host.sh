95654a9784906e383d580a66a92f0532c0f88626
Fusion <leemenhin@gmail.com>
2025-03-22 08:46:57 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -61,5 +61,5 @@ echo "Adding Title and SYNC data..."
 sed -i "14cTotal ad / tracking block list 屏蔽追踪广告总数: $(wc -l < block)" README.md  
 sed -i "16cTotal allowlist list 允许名单总数: $(wc -l < allow)" README.md 
 sed -i "24cUpdate 更新时间: $(date '+%Y-%m-%d')" README.md
-sed -i "48cNumber of Domain 域名数目： $(wc -l < block)" README.md
+sed -i "44cNumber of Domain 域名数目： $(wc -l < block)" README.md
 echo "Done!"
